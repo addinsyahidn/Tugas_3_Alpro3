2d4be19a4366bb0eb2b90b66cfed3373a8898530
addinsyahidn <addinsyahidn@gmail.com>
2026-04-15 18:31:17 +0700

    Update soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int n(0);
+
+    system("cls");
+
+    cin >> n;
+
+    for (char c : "Hore!")
+    // looping for untuk array (string = array char)
+        for (int i(0); i < n; i++) {
+            cout << c;
+            if (c == 'H' | c == 'r') break;
+            // jika H atau r cuman output sekali jadi langsung break looping for ke 2
+        }
+    return 0;
+}
