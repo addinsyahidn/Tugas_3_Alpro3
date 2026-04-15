198fd8f43e23d6f9d2f0bee04a07c49d263b20b0
addinsyahidn <addinsyahidn@gmail.com>
2026-04-15 18:45:08 +0700

    Update soal5.cpp

--- soal5.cpp
+++ soal5.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int bil(0), count(0);
+
+    system("cls");
+
+    cin >> bil;
+
+    for (int i(1); i <= bil; i++) {
+      // ngecek semua angka (tidak efisien)
+        if (bil%i==0) count++;
+        // kalo bisa dibagi count bertambah
+        if (count > 2 || bil < 2) {
+            cout << "Bilangan Bukan Prima";
+            return 0;
+        }
+    }
+    cout << "Bilangan Prima";
+    return 0;
+}
