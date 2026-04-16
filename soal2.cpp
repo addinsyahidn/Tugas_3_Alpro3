c80cadb68737202fdb7d92d6ea6c60851a92eae4
baktiarpurba542-commits <baktiarpurba542@gmail.com>
2026-04-16 21:22:15 +0700

    Update soal2.cpp

--- soal2.cpp
+++ soal2.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string kalimat;
+    char huruf;
+    int count = 0;
+
+    getline(cin, kalimat);
+    cin >> huruf;
+
+    string hasil = "";
+
+    for (int i = 0; i < kalimat.length(); i++) {
+        if (kalimat[i] != huruf) {
+            hasil += kalimat[i];
+        } else {
+            count++;
+        }
+    }
+
+    cout << hasil << endl;
+    cout << "Karakter terhapus = " << count << endl;
+
+    return 0;
+}
