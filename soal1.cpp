ab93d5cda00c8a6d54dc7a0c92602548ae51b546
baktiarpurba542-commits <baktiarpurba542@gmail.com>
2026-04-16 21:21:12 +0700

    Update soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    int angka = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cout << angka << " ";
+
+        if (i % 2 == 1)
+            angka += 5;
+        else
+            angka += 3;
+    }
+
+    return 0;
+}
